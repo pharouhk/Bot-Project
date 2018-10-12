ea5585877026b7ff0a1275ba4dfb5810ed8ced21
Gbadamosi Farouk <fgbadamosi@ymail.com>
2018-10-12 14:52:28 +0100

    New app folder

---
+++ app/main_bot.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+
+import requests
+import time
+import logging
+import argparse
+import os
+
+from requests.compat import urljoin
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+class BotHandler(object):
+    """
+        BotHandler is a class which implements all back-end of the bot.
+        It has tree main functions:
+            'get_updates' — checks for new messages
+            'send_message' – posts new message to user
+            'get_answer' — computes the most relevant on a user's question
+    """
+
+    def __init__(self, token, dialogue_manager):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.dialogue_manager = dialogue_manager
+
+    def get_updates(self, offset=None, timeout=30):
+        params = {"timeout": timeout, "offset": offset}
+        raw_resp = requests.get(urljoin(self.api_url, "getUpdates"), params)
+        try:
+            resp = raw_resp.json()
+        except json.decoder.JSONDecodeError as e:
+            print("Failed to parse response {}: {}.".format(raw_resp.content, e))
+            return []
+        if "result" not in resp:
+            return []
+        
+        return resp["result"]
+
+    def send_message(self, chat_id, text):
+        params = {"chat_id": chat_id, "text": text}
+        return requests.post(urljoin(self.api_url, "sendMessage"), params)
+
+
+
+    def get_answer(self, question):
+        if question == '/start':
+            return "Hi, I am your project bot. How can I help you today?"
+        elif question.lower() == 'hey' or question.lower() == 'hey?':
+            return 'Hi, there'
+        return self.dialogue_manager.generate_answer(question)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', type=str, default='')
+    return parser.parse_args()
+
+
+def is_unicode(text):
+    return len(text) == len(text.encode())
+
+
+class SimpleDialogueManager(object):
+    """
+    This is the simplest dialogue manager to test the telegram bot.
+    Your task is to create a more advanced one in dialogue_manager.py."
+    """
+    
+    def generate_answer(self, question): 
+        return "Hello, world!" 
+        
+
+def main():
+    args = parse_args()
+    token = args.token
+
+    if not token:
+        if not "TELEGRAM_TOKEN" in os.environ:
+            print("Please, set bot token through --token or TELEGRAM_TOKEN env variable")
+            return
+        token = os.environ["TELEGRAM_TOKEN"]
+
+    #################################################################
+    
+    # Your task is to complete dialogue_manager.py and use your 
+    # advanced DialogueManager instead of SimpleDialogueManager. 
+    
+    # This is the point where you plug it into the Telegram bot. 
+    # Do not forget to import all needed dependendies when you do so.
+    
+    from dialogue_manager import DialogueManager
+
+    paths = {
+    'INTENT_RECOGNIZER': 'intent_recognizer.pkl',
+    'TAG_CLASSIFIER': 'tag_classifier.pkl',
+    'TFIDF_VECTORIZER': 'tfidf_model.pkl',
+    'THREAD_EMBEDDINGS_FOLDER': 'thread_embeddings_by_tags_2',
+    'WORD_EMBEDDINGS': 'word_embeddings_reduced.pkl',
+}
+    advanced_manager = DialogueManager(paths)
+    bot = BotHandler(token, advanced_manager)
+    
+    ###############################################################
+    # logger = logging.getLogger()
+    # logger.setLevel(logging.DEBUG)
+
+    print("Ready to talk!")
+    offset = 0
+    while True:
+        updates = bot.get_updates(offset=offset)
+        for update in updates:
+            print("An update received.")
+            if "message" in update:
+                chat_id = update["message"]["chat"]["id"]
+                if "text" in update["message"]:
+                    text = update["message"]["text"]
+                    if is_unicode(text):
+                        print("Update content: {}".format(update))
+                        bot.send_message(chat_id, bot.get_answer(update["message"]["text"]))
+                    else:
+                        bot.send_message(chat_id, "Hmm, you are sending some weird characters to me...")
+            offset = max(offset, update['update_id'] + 1)
+        time.sleep(1)
+        # logger = logging.getLogger()
+        # logger.setLevel(logging.DEBUG)
+if __name__ == "__main__":
+    main()
