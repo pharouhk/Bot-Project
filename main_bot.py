5e361d64b8b8f0a73ca07a9cbb50e30bc880173d
Gbadamosi Farouk <fgbadamosi@ymail.com>
2018-10-15 17:29:08 +0100

    'new_updates'

--- main_bot.py
+++ main_bot.py
@@ -99,8 +99,8 @@ def main():
         # logger = logging.getLogger()
         # logger.setLevel(logging.DEBUG)
 if __name__ == "__main__":
-    # main()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host = '0.0.0.0', port=port)
+    main()
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host = '0.0.0.0', port=port)
 else:
     gunicorn_app = main()
